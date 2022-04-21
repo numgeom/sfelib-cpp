e532957a751623ad37cd9824df2d9808dd35fe01
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2022-04-21 09:04:13 -0400

    Removed time stamp

--- src/ElementTypes.hpp
+++ src/ElementTypes.hpp
@@ -2,7 +2,7 @@
 //
 //	Copyright (C) 2022 NumGeom Group at Stony Brook University
 //
-// This file was generated from update_etypeenum4cpp on 21-Apr-2022
+// This file was generated from update_etypeenum4cpp
 
 #ifndef SFE_ELEMENTTYPES_HPP
 #define SFE_ELEMENTTYPES_HPP
