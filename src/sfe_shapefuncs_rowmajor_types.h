c057f336bdd47b06cd70c3efb346ee827696d695
Xiangmin Jiao <xiangmin.jiao@stonybrook.edu>
2022-04-22 23:51:30 -0400

    Added row-major code

---
+++ src/sfe_shapefuncs_rowmajor_types.h
@@ -0,0 +1,15 @@
+// Copyright 2022 The NumGeom Group, Stony Brook University
+//
+// sfe_shapefuncs_rowmajor_types.h
+//
+// Code generation for function 'bar_2_sfunc'
+//
+
+#ifndef SFE_SHAPEFUNCS_ROWMAJOR_TYPES_H
+#define SFE_SHAPEFUNCS_ROWMAJOR_TYPES_H
+
+// Include files
+#include "rtwtypes.h"
+
+#endif
+// End of code generation (sfe_shapefuncs_rowmajor_types.h)
